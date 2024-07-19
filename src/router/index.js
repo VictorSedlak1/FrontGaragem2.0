05f06a8a449fc7453fd0a2520e8989388e4916c5
VictorSedlak1 <victorsedlak04@gmail.com>
2024-07-19 20:50:24 -0300

    Criação marca

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,9 @@ import HomeView from "../views/HomeView.vue";
 import CategoriaView from "../views/CategoriaView.vue";
 import LoginView from "../views/LoginView.vue";
 import LogoutView from "../views/LogoutView.vue";
+import MarcaView from "../views/MarcaView.vue";
+import AcessorioView from "../views/AcessorioView.vue";
+import CorView from "../views/CorView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -16,6 +19,21 @@ const router = createRouter({
       path: "/categorias",
       name: "categorias",
       component: CategoriaView,
+    },
+    {
+      path: "/marcas",
+      name: "marcas",
+      component: MarcaView,
+    },
+    {
+      path: "/acessorios",
+      name: "acessorios",
+      component: AcessorioView,
+    },
+    {
+      path: "/cores",
+      name: "cores",
+      component: CorView,
     },
     {
       path: "/login",
